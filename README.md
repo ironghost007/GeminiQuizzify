43961d2f6915b08fcc6572a0be6008796a614f61
Surya A E <aesurya05@gmail.com>
2024-08-23 20:29:52 -0400

    Update .gitignore to remove unwanted files

--- README.md
+++ README.md
@@ -1,1 +1,3 @@
 This is the quizzify task
+
+pip install langchain_community works over pip install langchain
---
+++ tasks/task_3/task_3.py
@@ -0,0 +1,6 @@
+import streamlit as st
+from langchain_community.document_loaders import PyPDFLoader
+import os
+import tempfile
+import uuid
+
